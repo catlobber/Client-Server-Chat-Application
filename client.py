2ec5a669ddbce40c13e2fc8f79e3760db6a40abe
Darryl Eason <darryleason4@gmail.com>
2025-03-31 16:51:07 -0500

    Cleaned up client code and added comments

--- client.py
+++ client.py
@@ -4,8 +4,7 @@ import threading
 HOST = ''  # Server address
 PORT = 12345
 headersize = 4
-
-running = True
+running = True #Set variable so we can stop recieve message across all threads
 # Receive messages from the server
 def receive_message(client_socket):
     global running
@@ -25,7 +24,7 @@ def client() -> None:
         # instantiate (create) socket and start server connection 
         socket_instance = socket.socket()
         socket_instance.connect((HOST, PORT))
-        rcv = threading.Thread(target=receive_message, args=[socket_instance]).start()
+        threading.Thread(target=receive_message, args=[socket_instance]).start()
 
         print(f'Client has connected.')
         print("Please input your username.")
@@ -34,14 +33,15 @@ def client() -> None:
             message = input()
 
             if message == '.exit':
-                message_header = f"{len(message):<{headersize}}".encode('utf-8')
+                message_header = f"{len(message):<{headersize}}".encode('utf-8') #Sends the .exit message with correct format
                 socket_instance.send(message_header + message.encode())
                 break
             
             # parse message to utf-8 and send message header first because server expects the size to be sent first
             message_header = f"{len(message):<{headersize}}".encode('utf-8')
             socket_instance.send(message_header + message.encode())
-        running = False
+
+        running = False #Tell all threads to stop running recieve message loop
         socket_instance.close()
         print("Client has been disconnected.")
         
