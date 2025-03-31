93d60b8965811f986c167433e0bcdbd983d47f16
Darryl Eason <darryleason4@gmail.com>
2025-03-31 16:47:24 -0500

    Modified server and client to connect and throw no errors after disconnection from client.

--- client.py
+++ client.py
@@ -1,39 +1,50 @@
 import socket
 import threading
 
-HOST = '127.0.0.1'  # Server address
-PORT = 65432
+HOST = ''  # Server address
+PORT = 12345
+headersize = 4
 
+running = True
 # Receive messages from the server
 def receive_message(client_socket):
-    while True:
+    global running
+    while running:
         try:
-            msg = client_socket.recv(1024).decode()
+            msg = client_socket.recv(headersize).decode()
+            if msg == '.exit':
+                print("Client disconnected successfully")
+                break
         except Exception as e:
             print(f'Error handling message from server: {e}')
             break
 
 def client() -> None:
-
+    global running
     try:
         # instantiate (create) socket and start server connection 
         socket_instance = socket.socket()
-        socket_instance.connect(HOST, PORT)
-        threading.Thread(target=receive_message, args=[socket_instance]).start()
+        socket_instance.connect((HOST, PORT))
+        rcv = threading.Thread(target=receive_message, args=[socket_instance]).start()
 
         print(f'Client has connected.')
-
+        print("Please input your username.")
         # read the user until the .exit prompt
         while True:
             message = input()
 
             if message == '.exit':
+                message_header = f"{len(message):<{headersize}}".encode('utf-8')
+                socket_instance.send(message_header + message.encode())
                 break
             
-            # parse message to utf-8
-            socket_instance.send(message.encode())
-
+            # parse message to utf-8 and send message header first because server expects the size to be sent first
+            message_header = f"{len(message):<{headersize}}".encode('utf-8')
+            socket_instance.send(message_header + message.encode())
+        running = False
         socket_instance.close()
+        print("Client has been disconnected.")
+        
     
     except Exception as e:
         print(f'Error connecting to server: {e}')
--- server.py
+++ server.py
@@ -5,16 +5,21 @@ import select
 def createwelcomingsocket(port):
         s = socket.socket()
         s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE,1)
         s.bind(('',port))
         s.listen()
         print("Welcoming Socket created and listening")
         return s
 
+welcomingsocket = createwelcomingsocket(12345)
+socketlist = [welcomingsocket]
+clients = {}
+headersize = 4
 
 def recieve_message(client_socket):
     try:
-        message_header = client_socket.recv(messagesize)
-        if not len(message_header):
+        message_header = client_socket.recv(headersize)
+        if not message_header:
             return False
         message_length = int(message_header.decode('utf-8').strip())
         return {'header': message_header, 'data': client_socket.recv(message_length)}
@@ -22,14 +27,11 @@ def recieve_message(client_socket):
     except:
         return False
 
-welcomingsocket = createwelcomingsocket(12345)
-socketlist = [welcomingsocket]
-clients = {}
-messagesize = 4
+
 
 while True:
- available, sending, errors = select.select(socketlist, clients, [])
- for connected in available:
+ read, _, exceptions = select.select(socketlist, [], socketlist)
+ for connected in read:
     if connected == welcomingsocket:
          client_socket, client_address = welcomingsocket.accept()
          user = recieve_message(client_socket)
@@ -41,9 +43,10 @@ while True:
     else:
         message = recieve_message(connected)
 
-        if message is False:
+        if message is False or message['data'].decode('utf-8') == '.exit':
             print("Closed connection from: {}".format(clients[connected]['data'].decode('utf-8')))
             socketlist.remove(connected)
+            connected.close()
             del clients[connected]
             continue
 
