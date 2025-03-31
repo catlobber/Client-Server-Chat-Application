aece5b1e22af4b50f09626af771be4aef4f2b535
Darryl Eason <darryleason4@gmail.com>
2025-03-31 15:39:53 -0500

    Finished server stuff

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import select
 
 def createwelcomingsocket(port):
         s = socket.socket()
+        s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
         s.bind(('',port))
         s.listen()
         print("Welcoming Socket created and listening")
@@ -32,4 +33,24 @@ while True:
     if connected == welcomingsocket:
          client_socket, client_address = welcomingsocket.accept()
          user = recieve_message(client_socket)
+         if user is False:
+             continue
+         socketlist.append(client_socket)
+         clients[client_socket] = user
+         print(f"Accepted connection from {client_address}, username: {user}")
+    else:
+        message = recieve_message(connected)
+
+        if message is False:
+            print("Closed connection from: {}".format(clients[connected]['data'].decode('utf-8')))
+            socketlist.remove(connected)
+            del clients[connected]
+            continue
+
+        user = clients[connected]
+        print(f'Received message from {user["data"].decode("utf-8")}: {message["data"].decode("utf-8")}')
+        for client_socket in clients:
+            if client_socket != connected:
+                client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
+
                   
