a2838934c4048a5e929831390d8bd99ade7c1fdf
Darryl Eason <darryleason4@gmail.com>
2025-03-31 15:21:59 -0500

    added stuff

--- server.py
+++ server.py
@@ -1,15 +1,35 @@
 
 import socket
+import select
 
-def createsockets(count,port):
-    opensockets = []
-    while count != 0:
+def createwelcomingsocket(port):
         s = socket.socket()
         s.bind(('',port))
         s.listen()
-        opensockets.append(socket)
-        print("Socket created and listening")
-        count = count - 1
-    return opensockets
+        print("Welcoming Socket created and listening")
+        return s
 
-createsockets(2,12345)
+
+def recieve_message(client_socket):
+    try:
+        message_header = client_socket.recv(messagesize)
+        if not len(message_header):
+            return False
+        message_length = int(message_header.decode('utf-8').strip())
+        return {'header': message_header, 'data': client_socket.recv(message_length)}
+
+    except:
+        return False
+
+welcomingsocket = createwelcomingsocket(12345)
+socketlist = [welcomingsocket]
+clients = {}
+messagesize = 4
+
+while True:
+ available, sending, errors = select.select(socketlist, clients, [])
+ for connected in available:
+    if connected == welcomingsocket:
+         client_socket, client_address = welcomingsocket.accept()
+         user = recieve_message(client_socket)
+                  
